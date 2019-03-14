5069072e13fd98691476f684d3f5d261d08e3157
lazyKEV <viveksinha.301@gmail.com>
2019-03-14 20:39:15 +0530

    removed flask_manager for time being

--- btattendance/__init__.py
+++ btattendance/__init__.py
@@ -1,11 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from btattendance.config import Config
 
 
 db = SQLAlchemy()
-migrate = Migrate()
 
 
 def create_app(config_class=Config):
@@ -13,10 +11,9 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
     db.init_app(app)
-    migrate.init_app(app, db)
 
-    from btattendance.students.views import students # noqa
-    from btattendance.teachers.views import teachers # noqa
+    from btattendance.students.views import students  # noqa
+    from btattendance.teachers.views import teachers  # noqa
     app.register_blueprint(students, url_prefix='/student')
     app.register_blueprint(teachers, url_prefix='/teacher')
 
