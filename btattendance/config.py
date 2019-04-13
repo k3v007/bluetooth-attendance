f921df05a26db38c5c21b7278a4c1f1d158198ec
lazyKEV <viveksinha.301@gmail.com>
2019-04-13 14:02:59 +0530

    added msg queue

--- btattendance/config.py
+++ btattendance/config.py
@@ -2,19 +2,25 @@ import os
 
 
 class Config:
+    # App settings
     DEBUG = False
+    TESTING = True
     SECRET_KEY = os.environ['SECRET_KEY']
     SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
-
+    # Main Settings
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ['EMAIL_USER']
     MAIL_PASSWORD = os.environ['EMAIL_PASS']
-
+    FLASKY_MAIL_SENDER = 'noreply@btattendance.com'
+    # Database settings
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Redis Settings
+    REDIS_URL = os.environ['REDIS_URL']
 
 
 class Dev(Config):
     DEBUG = True
-    TESTING = True
+    TESTING = False
+    MAIL_SUPPRESS_SEND = False
